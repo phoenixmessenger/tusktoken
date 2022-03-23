d25410235d9dd6d012cca5fce8bc3c184204e1d4
phoenixmessenger <42844921+phoenixmessenger@users.noreply.github.com>
2022-03-23 21:12:19 +0200

    added new section, button & fixed some elements in header, fonts etc (version 0.7 from 23/3/2022)

---
+++ scroll.js
@@ -0,0 +1,8 @@
+Jquery
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 100);
+    return false;
+});
